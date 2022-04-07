28d1fda06cec84ab681c5bfa81bccddfaafdac35
EMMALULULU <zlubc@connect.ust.hk>
2022-04-07 11:55:41 +0800

    customer information dispaly app

---
+++ customer/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import { Button } from "antd";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <Button>Button</Button>
+    </div>
+  );
+}
+
+export default App;
